111d19188281cd259770da418c88015d0f2297e6
JosephJvB <jo-van-bo@hotmail.com>
2018-01-04 10:23:33 +1300

    errors handled and had fun

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ function create () {
   })
 }
 
+const niceThings = ['lovely', 'beautiful', 'pretty', 'sexy', 'ravishing', 'radiant', 'patriotic', 'sweet-as', 'neat', 'handsome', 'graceful', 'comely', 'charming', 'foxy', 'boss-ass']
+
 console.log('welcome to my second visit to EDA')
 
 console.log('')
@@ -15,11 +17,11 @@ console.log('')
 listPicNames()
 
 function listPicNames () {
-  console.log('enter a number 1-4 to view a picture!')
+  console.log('enter a number 1-4 to view a picture! (q to quit)')
   fs.readdir('./data', (err, files) => {
     if (err) throw err
     else {
-      files.forEach((f, i) => console.log(`${i + 1}: ${f}`))
+      files.forEach((f, i) => console.log(`${i + 1}: ${f.split('.')[0]}`))
       readIt(files)
     }
   })
@@ -29,13 +31,34 @@ function readIt (files) {
   const rl = create()
   rl.prompt()
   rl.on('line', input => {
-    fs.readFile(`./data/${files[input - 1]}`, 'utf8', (err, data) => {
-      if (err) throw err
+    if (input === 'q') process.exit()
+    let pic = files[input - 1]
+    fs.readFile(`./data/${pic}`, 'utf8', (err, data) => {
+      if (err) errorHandler(rl)
       else {
+        const r = Math.floor(Math.random() * niceThings.length)
         console.log(data)
+        setTimeout(() => console.log(`what a ${niceThings[r]} ${pic.split('.')[0]}!`), 1000)
+        setTimeout(lottaLogs, 1002)
         rl.close()
-        setTimeout(listPicNames, 500)
+        setTimeout(listPicNames, 2000)
       }
     })
   })
+}
+
+function errorHandler (r) {
+  r.close()
+  console.log('')
+  console.log('whoops, something went wrong!')
+  lottaLogs()
+  setTimeout(() => console.log('please try again :)'), 700)
+  setTimeout(lottaLogs, 702)
+  setTimeout(listPicNames, 1200)
+}
+
+function lottaLogs () {
+  console.log('')
+  console.log('')
+  console.log('')
 }
